70f1634a53708692ac6ca8b1c997939ebab59051
Samantha Montgomery <32003691+montgomerysamantha@users.noreply.github.com>
2017-10-25 12:48:55 -0500

    Added sample matrix files and file path to Proj6.java
    Proj6.java now accepts command line arguments. The array is then manipulated into a string which is used to open a file connection. (Example: user types in "matrix1.txt", the program will now access the the file path "src/matrixInput/matrix1.txt."

---
+++ src/Matrix.java
@@ -0,0 +1,26 @@
+public class Matrix {
+    public void setElem(int row, int col, int value) {
+
+    }
+
+    public String toString() {
+        /* returns a string representation of the matrix */
+        return " ";
+    }
+
+    public Matrix plus(Matrix m) {
+        return null;
+    }
+
+    public Matrix minus(Matrix m) {
+        return null;
+    }
+
+    public Matrix times(Matrix m) {
+        return null;
+    }
+
+    public Matrix transpose() {
+        return null;
+    }
+}
